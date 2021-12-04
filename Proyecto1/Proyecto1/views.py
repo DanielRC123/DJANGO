7dac5401283ce5a45487b9a684355e8ef930a3c6
Daniel Roman Caro <danielrc@gmail.com>
2021-12-03 19:12:47 -0500

    a ver

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from django.http.response import HttpResponse
 from django.template import Template,Context
 
-
+class Pepo(object):
+    def __init__(self,nombre,apellido):
+        self.nombre = nombre
+        self.apellido = apellido
 def saludo(request):
     return HttpResponse("Hola ficha 2237520 Tgo ADSISISISI , Esta es la primera pagina con Django")
 
@@ -13,12 +16,14 @@ def despedida(request):
 
 def texto_en_html(request):
     fecha = datetime.now()
-    nombre = "PEPITO"
-    apellido = "RAMBO"
+    # nombre = "PEPITO"
+    # apellido = "RAMBO"
+    persona = Pepo("Nicolas","Ramirez")
+    tema_estudiar = ["Plantillas","Modelos","Formularios","Vistas","Despliegue"]
     doc_externo = open(("C:/Users/SENA/Desktop/Todo lo que habia en el escritorio esta aqui/Ficha 2237520/4 trimestre/Lunes_jueves_viernes/Construir_Sistema/Framework, Django/CODIGO/Proyecto1/template/miplantilla.html"))
     plt = Template(doc_externo.read())
     doc_externo.close()
-    ctx = Context({"Nombre_persona":nombre,"Apellido_persona":apellido,"Fecha":fecha})
+    ctx = Context({"Nombre_persona":persona.nombre,"Apellido_persona":persona.apellido,"Fecha":fecha, "temas":tema_estudiar})
 
     documento = plt.render(ctx)
 
